cdf2a5b102ed91fa38c12f9ceaaf1572571e0951
Madu Teixeira <m.eduardateixeirasilva@outlook.com>
2026-08-18 16:31:21 -0300

    Add files via upload

---
+++ revisaoPython/vetores.py
@@ -0,0 +1,12 @@
+nomes = ["Julia", "Maria", "Carla", "Enzo"]
+numeros = [33, 11, 22, 56]
+
+print(nomes[2])
+# append - enserir novo elemento no final da lista
+nomes.append("Lucas")
+# insert - inserir novo elemento em uma posição específica da lista
+numeros.insert(3, 50)
+# pop - remover elemento de uma posição específica da lista
+numeros.pop(1)
+# remove - remover elemento pelo conteúdo da lista
+nomes.remove("Carla")
