d465887684c40c32e84cc246ebd864754f8c6724
damasceno1997 <mayayaranunes@gmail.com>
2026-08-19 14:56:26 -0300

    vetores

--- revisaoPython/vetores.py
+++ revisaoPython/vetores.py
@@ -1,12 +1,42 @@
 nomes = ["Julia", "Maria", "Carla", "Enzo"]
-numeros = [33, 11, 22, 56]
+numeros = [33, 11, 22, 56, 58, 100, 33]
 
-print(nomes[2])
+# print(nomes[2])
 # append - enserir novo elemento no final da lista
-nomes.append("Lucas")
+# nomes.append("Lucas")
+
 # insert - inserir novo elemento em uma posição específica da lista
-numeros.insert(3, 50)
+# numeros.insert(3, 50)
+
 # pop - remover elemento de uma posição específica da lista
-numeros.pop(1)
+# numeros.pop(1)
+
 # remove - remover elemento pelo conteúdo da lista
-nomes.remove("Carla")
+# nomes.remove("Carla")
+
+# sort - ordena um vetor numérico em ordem crescente e um vetor do tipo string em ordem alfabética
+# numeros.sort()
+# print(numeros)
+# nomes.sort()
+# print(nomes)
+
+# reverse - inverte as posições do vetor
+
+# len - mede o valor total de um vetor
+# quantidade = len(numeros)
+# print(f"A quantidade de números é {quantidade}")
+
+# count - informa a quantidade de um valor específico
+# quantidade = numeros.count(33)
+# print(quantidade)
+
+# sum - Soma todos os valores de um vetor
+# total = sum(numeros)
+# print(total)
+
+# max - min - maior ou menor valor de um vetor
+# maxn = max(numeros)
+# print(maxn)
+# minn = min(numeros)
+# print(minn)
+
